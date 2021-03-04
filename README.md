fe5a56f8de470d146beb97dc3d242367b70c6a8f
梁亚辉 <liangyahui@didiglobal.com>
2021-03-04 16:34:15 +0800

    update

--- README.md
+++ README.md
@@ -3,3 +3,40 @@ A validate framework for js both node.js and browser
 
 ## 一个不带校验功能的校验库，以此来纪念我迷茫的这几天。
 [文档](/doc/document.md)
+
+```js
+const fs = require('fs')
+const Validator = require("./../index")
+
+Validator.addPlugin({
+  tagName: "required",
+  execFunc(field, value, opts) {
+    if (opts.tagValue === true && (value == undefined || value == null || value == "")) {
+      return false
+    }
+    return true
+  }
+})
+
+const v = new Validator({
+  field: [{
+    required: true,
+    message: "field is required"
+  }
+})
+
+console.log(JSON.stringify(v.validate({
+  field1: ""
+})))
+```
+输出
+```json
+[{
+	"field": "field",
+	"error": [{
+		"message": "field is required",
+		"tagName": "required",
+		"tagValue": true
+	}]
+}]
+```
--- demo/index.js
+++ demo/index.js
@@ -1,81 +1,22 @@
-const fs = require('fs')
 const Validator = require("./../index")
 
-Validator.addPlugin(Validator.DefaultPlugins.required)
 Validator.addPlugin({
-  tagName: "minSize",
-  message: "field4 with minSize 1",
-  execFunc: function (field, value, opts) {
-    return false
+  tagName: "required",
+  execFunc(field, value, opts) {
+    if (opts.tagValue === true && (value == undefined || value == null || value == "")) {
+      return false
+    }
+    return true
   }
 })
 
 const v = new Validator({
-  field1: [{
+  field: [{
     required: true,
-    message: "field1 is required"
-  },
-  {
-    Required: true,
-    message: "field1 is required"
-  }],
-  field2: [{
-    required: true,
-    message: "field2 is required",
-    messageFormat: function (field, value, opts) {
-      return `Message for [${field}] about value with ${value} from [${opts.tagName}]`
-    }
-  }],
-  field3: [{
-    required: true,
-    message: "field3 is required",
-    execFunc: function (field, value, opts) {
-      console.log(...arguments)
-      return false
-    }
-  }],
-  field4: [
-    {
-      required: true,
-      message: "field4 is required",
-    },
-    {
-      minSize: 1
-    }
-  ],
-  field5: [
-    {
-      type: "Object",
-      fields: {
-        field1: [{required: true}, {minSize: 1}]
-      }
-    },
-    {
-      required: true,
-      message: "field5 is required",
-    }
-  ],
-  field6: [
-    {
-      type: "Array",
-      fields: {
-        field1: [{required: true}, {minSize: 1}],
-        field2: [{required: true}, {minSize: 1}]
-      }
-    },
-    {
-      required: true,
-      message: "field6 is required",
-    }
-  ]
+    message: "field is required"
+  }]
 })
 
 console.log(JSON.stringify(v.validate({
-  field1: "test",
-  field2: "test",
-  field5: {
-  },
-  field6: [{field1: ""}]
+  field: "test"
 })))
-
-fs.readSync(process.stdin.fd, 1000, 0, 'utf8')
